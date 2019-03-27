ef5094b8406d86f6613313559ee345f361516c56
Marco Ferreira <marcoantonio00@outlook.com>
2019-03-27 18:54:25 -0300

    Consulta simples com query methods

--- src/main/java/com/marcoferreira/springbootmongo/repository/PostRepository.java
+++ src/main/java/com/marcoferreira/springbootmongo/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.marcoferreira.springbootmongo.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.marcoferreira.springbootmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
