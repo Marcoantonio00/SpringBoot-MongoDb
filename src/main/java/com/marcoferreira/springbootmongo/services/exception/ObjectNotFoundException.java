3c141250d13b9ebb32abb34f65f6cc02c829512e
Marco Ferreira <marcoantonio00@outlook.com>
2019-03-27 12:21:42 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/marcoferreira/springbootmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.marcoferreira.springbootmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
